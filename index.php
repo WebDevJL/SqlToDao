526c1d91ebc99a08a776aea3f3979850358bf8a3
FWA_JL <j.litzler@fieldworkassistant.net>
2014-06-05 10:55:49 +0200

    List the column names

--- index.php
+++ index.php
@@ -8,7 +8,16 @@ if ($stmt->rowCount() > 0) {
   $tables = $stmt->fetchAll(PDO::FETCH_NUM);
   foreach ($tables as $table) {
     var_dump($table);
+    echo '<br />';
     $table_name = $table[0];
-    // do something
+    get_table_column_names($table[0], $DBH);
   }
+}
+
+function get_table_column_names($table, $dbh) {
+  $q = $dbh->prepare('DESCRIBE '.$table.';');
+  $q->execute();
+  $table_fields = $q->fetchAll(PDO::FETCH_COLUMN);
+  echo '<br />';
+  var_dump($table_fields);
 }
